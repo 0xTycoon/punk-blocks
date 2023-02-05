e0bf3408d7fc23227b5737936113f5f49d629696
0xTycoon <0xTycoon@users.noreply.github.com>
2023-02-05 23:47:25 +0900

    add stats

--- generator/data.go
+++ generator/data.go
@@ -39,115 +39,115 @@ var params = `0,Male 1,m,0,598
 19,Nerd Glasses,m,6,572
 20,Regular Shades,m,6,527
 21,Knitted Cap,m,11,419
-22,Shadow Beard,m,4
-23,Frown,m,8
-24,Cap Forward,m,11
-25,Goat,m,4
-26,Mole,m,2
-27,Purple Hair,m,3
-28,Small Shades,m,6
-29,Shaved Head,m,3
-30,Classic Shades,m,6
-31,Vape,m,9
-32,Silver Chain,m,12
-33,Smile,m,8
-34,Big Shades,m,6
-35,Mohawk Thin,m,3
-36,Beanie,m,11
-37,Cap,m,11
-38,Clown Eyes Green,m,5
-39,Normal Beard Black,m,4
-40,Medical Mask,m,9
-41,Normal Beard,m,4
-42,VR,m,6
-43,Eye Patch,m,5
-44,Wild Hair,m,3
-45,Top Hat,m,11
-46,Bandana,m,11
-47,Handlebars,m,4
-48,Frumpy Hair,m,3
-49,Crazy Hair,m,3
-50,Police Cap,m,11
-51,Buck Teeth,m,8
-52,Do-rag,m,11
-53,Front Beard,m,4
-54,Spots,m,2
-55,Big Beard,m,4
-56,Vampire Hair,m,3
-57,Peak Spike,m,3
-58,Chinstrap,m,4
-59,Fedora,m,11
-60,Earring,m,10
-61,Horned Rim Glasses,m,6
-62,Headband,m,11
-63,Pipe,m,9
-64,Messy Hair,m,3
-65,Front Beard Dark,m,4
-66,Hoodie,m,11
-67,Gold Chain,m,12
-68,Muttonchops,m,4
-69,Stringy Hair,m,3
-70,Eye Mask,m,6
-71,3D Glasses,m,6
-72,Clown Eyes Blue,m,5
-73,Mohawk,m,3
-74,Pilot Helmet,f,11
-75,Tassle Hat,f,11
-76,Hot Lipstick,f,8
-77,Blue Eye Shadow,f,5
-78,Straight Hair Dark,f,3
-79,Choker,f,12
-80,Crazy Hair,f,3
-81,Regular Shades,f,6
-82,Wild Blonde,f,3
-83,3D Glasses,f,6
-84,Mole,f,2
-85,Wild White Hair,f,3
-86,Spots,f,2
-87,Frumpy Hair,f,3
-88,Nerd Glasses,f,6
-89,Tiara,f,11
-90,Orange Side,f,3
-91,Red Mohawk,f,3
-92,Messy Hair,f,3
-93,Clown Eyes Blue,f,5
-94,Pipe,f,9
-95,Wild Hair,f,3
-96,Purple Eye Shadow,f,5
-97,Stringy Hair,f,3
-98,Dark Hair,f,3
-99,Eye Patch,f,6
-100,Blonde Short,f,3
-101,Classic Shades,f,6
-102,Eye Mask,f,6
-103,Clown Hair Green,f,3
-104,Cap,f,11
-105,Medical Mask,f,9
-106,Bandana,f,11
-107,Purple Lipstick,f,8
-108,Clown Nose,f,7
-109,Headband,f,11
-110,Pigtails,f,3
-111,Straight Hair Blonde,f,3
-112,Knitted Cap,f,11
-113,Clown Eyes Green,f,5
-114,Cigarette,f,9
-115,Welding Goggles,f,6
-116,Mohawk Thin,f,3
-117,Gold Chain,f,12
-118,VR,f,6
-119,Vape,f,9
-120,Pink With Hat,f,3
-121,Blonde Bob,f,3
+22,Shadow Beard,m,4,526
+23,Frown,m,8,261
+24,Cap Forward,m,11,254
+25,Goat,m,4,295
+26,Mole,m,2,644
+27,Purple Hair,m,3,165
+28,Small Shades,m,6,378
+29,Shaved Head,m,3,300
+30,Classic Shades,m,6,502
+31,Vape,m,9,272
+32,Silver Chain,m,12,156
+33,Smile,m,8,238
+34,Big Shades,m,6,535
+35,Mohawk Thin,m,3,441
+36,Beanie,m,11,44
+37,Cap,m,11,351
+38,Clown Eyes Green,m,5,382
+39,Normal Beard Black,m,4,289
+40,Medical Mask,m,9,175
+41,Normal Beard,m,4,292
+42,VR,m,6,332
+43,Eye Patch,m,5,461
+44,Wild Hair,m,3,447
+45,Top Hat,m,11,115
+46,Bandana,m,11,481
+47,Handlebars,m,4,263
+48,Frumpy Hair,m,3,442
+49,Crazy Hair,m,3,414
+50,Police Cap,m,11,203
+51,Buck Teeth,m,8,78
+52,Do-rag,m,11,300
+53,Front Beard,m,4,273
+54,Spots,m,2,124
+55,Big Beard,m,4,146
+56,Vampire Hair,m,3,147
+57,Peak Spike,m,3,303
+58,Chinstrap,m,4,282
+59,Fedora,m,11,186
+60,Earring,m,10,2459
+61,Horned Rim Glasses,m,6,535
+62,Headband,m,11,406
+63,Pipe,m,9,317
+64,Messy Hair,m,3,460
+65,Front Beard Dark,m,4,260
+66,Hoodie,m,11,259
+67,Gold Chain,m,12,169
+68,Muttonchops,m,4,303
+69,Stringy Hair,m,3,463
+70,Eye Mask,m,6,293
+71,3D Glasses,m,6,286
+72,Clown Eyes Blue,m,5,384
+73,Mohawk,m,3,441
+74,Pilot Helmet,f,11,54
+75,Tassle Hat,f,11,178
+76,Hot Lipstick,f,8,696
+77,Blue Eye Shadow,f,5,266
+78,Straight Hair Dark,f,3,148
+79,Choker,f,12,48
+80,Crazy Hair,f,3,414
+81,Regular Shades,f,6,527
+82,Wild Blonde,f,3,144
+83,3D Glasses,f,6,286
+84,Mole,f,2,644
+85,Wild White Hair,f,3,136
+86,Spots,f,2,124
+87,Frumpy Hair,f,3,442
+88,Nerd Glasses,f,6,572
+89,Tiara,f,11,55
+90,Orange Side,f,3,68
+91,Red Mohawk,f,3,147
+92,Messy Hair,f,3,460
+93,Clown Eyes Blue,f,5,384
+94,Pipe,f,9,317
+95,Wild Hair,f,3,447
+96,Purple Eye Shadow,f,5,262
+97,Stringy Hair,f,3,463
+98,Dark Hair,f,3,157
+99,Eye Patch,f,6,461
+100,Blonde Short,f,3,129
+101,Classic Shades,f,6,502
+102,Eye Mask,f,6,293
+103,Clown Hair Green,f,3,148
+104,Cap,f,11,351
+105,Medical Mask,f,9,175
+106,Bandana,f,11,481
+107,Purple Lipstick,f,8,655
+108,Clown Nose,f,7,212
+109,Headband,f,11,406
+110,Pigtails,f,3,94
+111,Straight Hair Blonde,f,3,144
+112,Knitted Cap,f,11,419
+113,Clown Eyes Green,f,5,382
+114,Cigarette,f,9,961
+115,Welding Goggles,f,6,86
+116,Mohawk Thin,f,3,Mohawk Thin
+117,Gold Chain,f,12,169
+118,VR,f,6,332
+119,Vape,f,9,272
+120,Pink With Hat,f,3,95
+121,Blonde Bob,f,3,147
 122,Mohawk,f,3,441
-123,Big Shades,f,6
-124,Earring,f,10
-125,Green Eye Shadow,f,5
-126,Straight Hair,f,3
-127,Rosy Cheeks,f,1
-128,Half Shaved,f,3
-129,Mohawk Dark,f,3,429
-130,Black Lipstick,f,8
-131,Horned Rim Glasses,f,6
-132,Silver Chain,f,12
+123,Big Shades,f,6,535
+124,Earring,f,10,2459
+125,Green Eye Shadow,f,5,271
+126,Straight Hair,f,3,151
+127,Rosy Cheeks,f,1,128
+128,Half Shaved,f,3,147
+129,Mohawk Dark,f,3,429,429
+130,Black Lipstick,f,8,617
+131,Horned Rim Glasses,f,6,535
+132,Silver Chain,f,12,156
 `
