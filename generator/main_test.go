df58a69d801b249850e1950de8179fde601079c6
0xTycoon <0xTycoon@users.noreply.github.com>
2023-02-09 15:49:33 +0900

    refactor 3

--- generator/main_test.go
+++ generator/main_test.go
@@ -53,7 +53,25 @@ type block struct {
 	freq int
 	sex  string
 	name string
+	cat  int
 }
+
+func getDistributionData() map[string][]int {
+	distribution := make(map[string][]int)
+	csvReader := csv.NewReader(bytes.NewReader([]byte(distributionData)))
+	records, _ := csvReader.ReadAll()
+	for i := range records {
+		var counts []int
+		for n := 0; n < 8; n++ {
+			v, _ := strconv.Atoi(records[i][n+5])
+			counts = append(counts, v)
+		}
+		distribution[records[i][1]] = counts
+	}
+	return distribution
+}
+
+var distribution map[string][]int
 
 func TestGenerator(t *testing.T) {
 	var allBlocks blocks
@@ -63,10 +81,12 @@ func TestGenerator(t *testing.T) {
 		fmt.Println(err)
 		return
 	}
+	distribution = getDistributionData()
 	csvReader := csv.NewReader(bytes.NewReader([]byte(params)))
 	records, err := csvReader.ReadAll()
 	male := make(map[int][]block)
 	female := make(map[int][]block)
+	attributes := make(map[int]block)
 	bases := make(map[int][]block)
 
 	for i := 0; i < 133; i++ {
@@ -80,6 +100,7 @@ func TestGenerator(t *testing.T) {
 				frq,
 				records[i][2],
 				records[i][1],
+				cat,
 			})
 		} else if records[i][2] == "m" {
 			male[cat] = append(male[cat], block{
@@ -87,22 +108,37 @@ func TestGenerator(t *testing.T) {
 				frq,
 				"m",
 				records[i][1],
+				cat,
 			})
+			attributes[id] = block{
+				allBlocks.getPunkBlock(id),
+				frq,
+				"m",
+				records[i][1],
+				cat,
+			}
 		} else {
 			female[cat] = append(female[cat], block{
 				allBlocks.getPunkBlock(id),
 				frq,
 				"f",
 				records[i][1],
+				cat,
 			})
-
+			attributes[id] = block{
+				allBlocks.getPunkBlock(id),
+				frq,
+				"f",
+				records[i][1],
+				cat,
+			}
 		}
 
 	}
 	sum := 0
 	total := 0
-	for i := 0; i < 1; i++ {
-		counts, zbcount := pickCollection(bases, female, male)
+	for i := 0; i < 100; i++ {
+		counts, zbcount := pickCollection(bases, female, male, attributes)
 		if zbcount > 0 {
 			total += 1
 		}
@@ -137,6 +173,7 @@ func pickCollection(
 	base map[int][]block,
 	female map[int][]block,
 	male map[int][]block,
+	attributes map[int]block,
 ) (map[int]int, int) {
 	var chosenAtt []block
 	counts := make(map[int]int)
@@ -146,19 +183,15 @@ func pickCollection(
 	for i := 0; i < 10000; i++ {
 		chosenBase := pickBase(base, r1)
 		count := pickCount(r1)
-		if chosenBase.sex == "f" {
-			attrCounts["m"]++
-			chosenAtt = pickPunkToAttributes2(female, r1, count, chosenBase.name)
-		} else {
-			attrCounts["f"]++
-			chosenAtt = pickPunkToAttributes2(male, r1, count, chosenBase.name)
-		}
+
+		chosenAtt = pickPunkToAttributes(attributes, r1, count, chosenBase)
+
 		for moo := range chosenAtt {
 			attrCounts[chosenAtt[moo].name]++
 		}
-		if chosenBase.name == "Alien" { // || chosenBase.name == "Ape" || chosenBase.name == "Alien"
+		if chosenBase.name == "7" { // || chosenBase.name == "Ape" || chosenBase.name == "Alien"
 			for a := range chosenAtt {
-				if chosenAtt[a].name == "Cowboy Hat" {
+				if chosenAtt[a].name == "Beanie" {
 					zbcount++
 				}
 			}
@@ -176,6 +209,7 @@ func pickCollection(
 var hairExcluded int
 
 func isExcluded(base string, cat int) bool {
+	return false
 	if base == "Alien" && cat == 3 {
 		hairExcluded++
 		return true // hair excluded from alien
@@ -185,100 +219,53 @@ func isExcluded(base string, cat int) bool {
 
 var attrCounts = make(map[string]int)
 
-func pickPunkToAttributes2(
-	set map[int][]block,
+func pickPunkToAttributes(
+	blocks map[int]block,
 	r1 *rand.Rand,
 	desiredCount int,
-	base string) []block {
+	base block,
+) []block {
 	var chosenAtt []block
 	if desiredCount == 0 {
 		return chosenAtt
 	}
+	catPicks := make(map[int]bool)
+	rolls := 0
 	for {
-		i := r1.Intn(len(set)) + 1
-		if _, ok := set[i]; !ok {
-			//fmt.Println("zeroooo!") // eg. f has no facial hair
+		i := r1.Intn(122) + 11
+		pick := blocks[i]
+		if pick.sex != base.sex {
 			continue
 		}
-		j := r1.Intn(len(set[i]))
-		n2 := r1.Intn(10000)
-		if set[i][j].freq >= n2 {
-			chosenAtt = append(chosenAtt, set[i][j])
-			if _, ok := attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)]; ok {
-				//	attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)]++
-			} else {
-				//	attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)] = 1
-			}
+
+		if _, ok := catPicks[pick.cat]; ok {
+			rolls++
+			continue
 		}
+		n2 := r1.Intn(10000)
+		if punkProbability(pick, base) >= n2 {
+			chosenAtt = append(chosenAtt, pick)
+			catPicks[pick.cat] = true // remember that this category was picked
+		}
+		rolls++
 		if len(chosenAtt) == desiredCount {
 			return chosenAtt
 		}
 	}
-	//return chosenAtt
 }
 
-func pickPunkToAttributes(
-	set map[int][]block,
-	r1 *rand.Rand,
-	desiredCount int,
-	base string) []block {
-
-	var chosenAtt []block
-	if desiredCount == 0 {
-		return chosenAtt
+func punkProbability(pick block, base block) int {
+	ret := 0
+	i := 0
+	if base.sex == "f" {
+		i += 4
 	}
-	n2 := r1.Intn(10000)
-
-	i := r1.Intn(len(set)) + 1
-	catRolls := 0
-	for {
-		if _, ok := set[i]; !ok {
-			//fmt.Println("zeroooo!")
-			i++
-			continue
-		}
-
-		j := r1.Intn(len(set[i]))
-		attRolls := 0
-		catRolls++
-
-		//fmt.Println("cat roll:", catRolls, ", i:", i)
-		for {
-			n2 = r1.Intn(10000)
-			//fmt.Println("attRoll, ", attRolls, "cat len", len(set[i]), " n:", set[i][j].name, " ", n2)
-
-			if set[i][j].freq >= n2 && !isExcluded(base, i) {
-				chosenAtt = append(chosenAtt, set[i][j])
-				if _, ok := attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)]; ok {
-					attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)]++
-				} else {
-					attrCounts[set[i][j].name+strconv.Itoa(set[i][j].freq)] = 1
-				}
-				//fmt.Println("picked!", set[i][j])
-				if len(chosenAtt) == desiredCount {
-					return chosenAtt
-				}
-
-				attRolls = 0
-				j = 0
-				break // finish rolling this category
-			}
-			j++
-			attRolls++
-			if attRolls >= len(set[i]) {
-				break
-				//i = 1
-			}
-
-			if j >= len(set[i]) {
-				j = 0
-			}
-		}
-		i++
-		if i > 12 {
-			i = 1 // wrap around the categories
-		}
+	n, _ := strconv.Atoi(base.name)
+	i += n - 1
+	if v, ok := distribution[pick.name]; ok {
+		return v[i]
 	}
+	return ret
 }
 
 var counts = [8]int{8, 333, 3560, 4501, 1420, 166, 11, 1}
@@ -300,7 +287,6 @@ func pickCount(r1 *rand.Rand) int {
 func pickBase(base map[int][]block, r1 *rand.Rand) block {
 	var chosenBase block
 	i := r1.Intn(len(base[0])) // random index (i) to start from
-	//fmt.Println("i is:", i)
 	for {
 		n2 := r1.Intn(10000)
 		if base[0][i].freq >= n2 {
@@ -312,6 +298,5 @@ func pickBase(base map[int][]block, r1 *rand.Rand) block {
 			i = 0
 		}
 	}
-	//fmt.Println("chosen base:", chosenBase)
 	return chosenBase
 }
